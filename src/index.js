f48686834099bf6b10631e0ac73e9119934a3224
Baptiste Raymond <baptiste@guidap.com>
2022-02-15 18:16:50 +0100

    added from cust-delivery

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/** start the app :) */
+function bootstrap() {
+    customerSpecialFeature();
+    return "it's started!"
+}
+
+function customerSpecialFeature(){
+    console.log('customer feature 1 added from cust-delivery');
+}
+
+bootstrap();
