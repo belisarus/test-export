7501b0c6570ce6b05c4aa06752687a020334ff20
Baptiste Raymond <baptiste@guidap.com>
2022-02-15 18:21:29 +0100

    added from develop

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 /** start the app :) */
 function bootstrap() {
+    developFeature3 ();
     return "it's started!"
+}
+
+function developFeature3 (){
+    console.log('develop feature 3');
 }
 
 bootstrap();
